9e94ce6a3347e6531dc9f02652e170ab1f1b66c0
priyaanumzn2012-beep <priya.anu.mzn2012@gmail.com>
2026-02-01 14:04:05 +0530

    Create 02_table_creation.sql

---
+++ 02_table_creation.sql
@@ -0,0 +1,10 @@
+CREATE TABLE orders (
+    order_id INT,
+    order_date DATE,
+    product_id VARCHAR(10),
+    defect_type VARCHAR(20),
+    warehouse VARCHAR(5),
+    quantity INT,
+    status VARCHAR(15),
+    employee_id VARCHAR(10)
+);
