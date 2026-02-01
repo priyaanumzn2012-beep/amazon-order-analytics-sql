a239e23621dd6aab71cc9e003b5059a79515359e
priyaanumzn2012-beep <priya.anu.mzn2012@gmail.com>
2026-02-01 14:08:10 +0530

    Add sample data to orders table

---
+++ 03_data_insertion.sql
@@ -0,0 +1,5 @@
+INSERT INTO orders (order_id, customer_id, order_date, order_amount, order_status)
+VALUES
+(1, 101, '2024-01-01', 1200, 'Delivered'),
+(2, 102, '2024-01-05', 800, 'Cancelled'),
+(3, 103, '2024-01-10', 1500, 'Delivered');
